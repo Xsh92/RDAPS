bba8e9aec2ca2624e7d629101cec0f9a2a7e736a
Xsh92 <simxsh@gmail.com>
2014-10-03 16:14:52 +0800

    V0.15
    Faggot

---
+++ www/js/main.js
@@ -0,0 +1,7 @@
+function openBrowser() {
+    var ref= window.open('http://www.curtin.edu.my', '_blank', 'location=yes');
+    ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+    ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+    ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+    ref.addEventListener('exit', function(event) { alert(event.type); });
+}
