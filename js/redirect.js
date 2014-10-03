09340ae1a14c864d9c217e1ccdec850f509abfe1
Xsh92 <simxsh@gmail.com>
2014-10-03 16:55:43 +0800

    V0.17
    Dummy

--- js/redirect.js
+++ js/redirect.js
@@ -1,5 +1,5 @@
 function openBrowser() {
-    var ref = window.open('http://www.curtin.edu.my', '_blank');
+    var ref = window.open('http://www.curtin.edu.my', '_blank', 'location=yes');
     ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
     ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
     ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
@@ -7,7 +7,7 @@ function openBrowser() {
 }
 
 function openExtBrowser() {
-    var ref1 = window.open('http://moodle.curtin.edu.my', '_system');
+    var ref1 = window.open('http://moodle.curtin.edu.my', '_system', 'location=yes');
     ref1.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
     ref1.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
     ref1.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
